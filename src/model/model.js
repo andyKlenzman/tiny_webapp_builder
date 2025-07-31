e7db9ebc9ba1ac976b102a5b9a4cae581deb70eb
Andy Klenzman <andyklenz@gmail.com>
2025-07-31 08:06:58 +0200

    Refactor model

--- src/model/model.js
+++ src/model/model.js
@@ -1,9 +1,5 @@
 import { DB } from "./dataAccess/dataAccessInterface";
 
-//////////////////////////////////////////////////////
-// Constants
-//////////////////////////////////////////////////////
-// TODO: Verorten mich woanders!! 
 export const COLLECTIONS = {
   GROUPS: "groups",
 };
@@ -12,66 +8,81 @@ export const VIEW_MODES = {
   STREAKS: "Streaks",
   EDIT_TIMESTAMPS: "Edit Timestamps",
 };
+
 //////////////////////////////////////////////////////
-// Model
+// State & View State
 //////////////////////////////////////////////////////
-// Koennte ich irgendwie die schichten hier trennn, also anwendeungen die aufm base ebende gebaut werden haben ihre eignenen schichte?
+const state = {
+  groups: {},
+  selectedGroups: [],
+  selectedTimestamps: {},
+};
 
-// could have controller as the entry point and expose places in DOM where other things could interate. ENtkoppeln streaks completely from tracking, in the sense that streaks would be only a number that takes in time.
+let currentView = VIEW_MODES.STREAKS;
 
-// I could structure this as models inside of models
-export const Model = {
-  // TODO: entweder von API ausschliesen oder get state loeschen
-  state: {
-    groups: {}, // TODO: Consider making this an array??
-    selectedGroups: [],
-    selectedTimestamps: [],
-    currentView: VIEW_MODES.STREAKS,
+//////////////////////////////////////////////////////
+// Helpers
+//////////////////////////////////////////////////////
+
+const syncGroups = async () => {
+  state.groups = await DB.getAll(COLLECTIONS.GROUPS);
+  return JSON.parse(JSON.stringify({ ...state, currentView }));
+};
+
+//////////////////////////////////////////////////////
+// ViewState
+//////////////////////////////////////////////////////
+
+const viewState = {
+  changeCurrentView: (newMode) => {
+    currentView = newMode;
+    return currentView;
   },
+  getCurrentView: () => currentView,
+};
 
-  async init() {
-    return this.getState();
-  },
+//////////////////////////////////////////////////////
+// Group Logic
+//////////////////////////////////////////////////////
 
-  async getState() {
-    this.state.groups = await DB.getAll(COLLECTIONS.GROUPS);
-    return JSON.parse(JSON.stringify(this.state));
-  },
-  // TODO: cache me in browser
-  changeCurrentView(newMode) {
-    Model.state.currentView = newMode;
-    return Model.state.currentView;
-  },
-
+const groupStore = {
   async addGroup(groupName) {
-    console.log("addGroup: name: ", groupName);
     const group = { groupName, timestamps: [] };
     const { id } = await DB.add(COLLECTIONS.GROUPS, group);
-
-    this.state.groups[id] = group;
-    console.log("addGroup: state.groups:", this.state.groups);
+    state.groups[id] = group;
     return id;
   },
 
   async deleteSelectedGroups() {
-    this.state.selectedGroups.forEach(async (id) => {
-      console.log(id);
+    for (const id of state.selectedGroups) {
       await DB.deleteById(COLLECTIONS.GROUPS, id);
-      delete this.state.groups[id];
-    });
-    this.state.selectedGroups = [];
-    return await this.getState();
+      delete state.groups[id];
+    }
+    state.selectedGroups = [];
+    return await syncGroups();
   },
 
-  // optional parameter that is deafult to nuill and checks and sets to default tim ei if nullor param is in functiona itself
+  toggleGroupSelection(groupId) {
+    const index = state.selectedGroups.indexOf(groupId);
+    if (index === -1) {
+      state.selectedGroups.push(groupId);
+    } else {
+      state.selectedGroups.splice(index, 1);
+    }
+  },
+};
+
+//////////////////////////////////////////////////////
+// Timestamp Logic
+//////////////////////////////////////////////////////
+
+const timestampStore = {
   async addTimestampToSelectedGroups(timestamp = new Date().toISOString()) {
     let updatedGroups = [];
-    // TODO: Is this magic scope refactor worthy
-    for (const id of this.state.selectedGroups) {
+
+    for (const id of state.selectedGroups) {
       let doc = await DB.getById(COLLECTIONS.GROUPS, id);
       doc.timestamps.push(timestamp);
-
-      // TODO: uberlegen ob ein update and return funktion wurde wiederverwendbarkeit erhoehern
       await DB.update(COLLECTIONS.GROUPS, id, doc);
       updatedGroups.push({ id, timestamps: [...doc.timestamps] });
     }
@@ -80,58 +91,54 @@ export const Model = {
   },
 
   async deleteSelectedTimestamps() {
-    console.log(this.state.selectedTimestamps);
-    const selected = this.state.selectedTimestamps;
+    for (const groupId in state.selectedTimestamps) {
+      if (!state.groups[groupId]) continue;
+      if (!state.selectedTimestamps[groupId]?.length) continue;
 
-    for (const groupId in selected) {
-      // Sanity check: skip if groupId does not exist in current groups
-      if (!this.state.groups[groupId]) continue;
-
-      // Check that there are timestamps selected for the currently iterated group
-      if (!selected[groupId] || !selected[groupId].length) continue;
       let doc = await DB.getById(COLLECTIONS.GROUPS, groupId);
-
-      console.log("deleteSelectedTimestamps: before", doc);
       doc.timestamps = doc.timestamps.filter(
-        (ts) => !selected[groupId].includes(ts)
+        (ts) => !state.selectedTimestamps[groupId].includes(ts)
       );
-
       await DB.update(COLLECTIONS.GROUPS, groupId, doc);
-
-      console.log("deleteSelectedTimestamps: after", doc);
     }
 
-    this.state.selectedTimestamps = {}; // Reset to empty object
-    return await this.getState();
+    state.selectedTimestamps = {};
+    return await syncGroups();
   },
 
-  toggleGroupSelection(groupId) {
-    console.log("toggleGroupSelection: SelectedIdd: ", groupId);
-    const idx = this.state.selectedGroups.indexOf(groupId);
-    if (idx === -1) {
-      this.state.selectedGroups.push(groupId);
-    } else {
-      this.state.selectedGroups.splice(idx, 1);
-    }
-
-    console.log("toggleGroupSelection: selected: ", this.state.selectedGroups);
-  },
   toggleTimestampSelection(groupId, timestamp) {
-    if (!this.state.selectedTimestamps[groupId]) {
-      this.state.selectedTimestamps[groupId] = [];
+    if (!state.selectedTimestamps[groupId]) {
+      state.selectedTimestamps[groupId] = [];
     }
 
-    const idx = this.state.selectedTimestamps[groupId].indexOf(timestamp);
-
-    if (idx === -1) {
-      this.state.selectedTimestamps[groupId].push(timestamp);
+    const index = state.selectedTimestamps[groupId].indexOf(timestamp);
+    if (index === -1) {
+      state.selectedTimestamps[groupId].push(timestamp);
     } else {
-      this.state.selectedTimestamps[groupId].splice(idx, 1);
+      state.selectedTimestamps[groupId].splice(index, 1);
     }
-
-    console.log(
-      "toggleTimestampSelection: selected: ",
-      this.state.selectedTimestamps
-    );
   },
+};
+
+//////////////////////////////////////////////////////
+// Model Entry Point
+//////////////////////////////////////////////////////
+
+export const Model = {
+  init: () => syncGroups(),
+
+  getState: () => syncGroups(),
+
+  getCurrentView: viewState.getCurrentView,
+  changeCurrentView: viewState.changeCurrentView,
+
+  addGroup: groupStore.addGroup,
+  deleteSelectedGroups: groupStore.deleteSelectedGroups,
+  toggleGroupSelection: groupStore.toggleGroupSelection,
+
+  addTimestampToSelectedGroups: timestampStore.addTimestampToSelectedGroups,
+  deleteSelectedTimestamps: timestampStore.deleteSelectedTimestamps,
+  toggleTimestampSelection: timestampStore.toggleTimestampSelection,
+
+  state,
 };
